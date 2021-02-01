b02892a0fcd9e09a4651f9f126e5a69aaf49da14
Mathieu Kempe <mathieukempe@gmail.com>
2021-02-01 12:47:22 +1100

    bump version

--- PexelsNet/Properties/AssemblyInfo.cs
+++ PexelsNet/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mathieu Kempe")]
 [assembly: AssemblyProduct("PexelsNet")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
